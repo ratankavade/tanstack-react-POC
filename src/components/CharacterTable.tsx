90ca958eddea86764740329db6d708de64571b4f
Sanjay Tapase <sanjaytapase@Sanjays-Macbook.local>
2025-06-12 18:16:42 +0530

    show active page on pagination

--- src/components/CharacterTable.tsx
+++ src/components/CharacterTable.tsx
@@ -139,7 +139,11 @@ import { Link } from '@tanstack/react-router'
               <button
                 key={p}
                 onClick={() => goToPage(p)}
-                className='class="flex items-center justify-center px-2 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"'
+                className={
+                  page === p
+                    ? 'z-10 flex items-center justify-center px-2 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+                    : 'flex items-center justify-center px-2 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                }
               >
                 {p}
               </button>
